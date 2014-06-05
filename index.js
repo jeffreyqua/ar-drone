878cffb53294febae1cce9155e27fb8c0852c880
ryan-ko <ryanko.780513@gmail.com>
2014-06-05 09:32:26 -0700

    Organized codes & added comments to configs

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+// Init Air Drone API
 var arDrone = require('ar-drone');
+// Init Air Drone Client
 var client = arDrone.createClient();
-//client.createRepl();
+// Init Air Drone Controls
 var control = arDrone.createUdpControl();
-
+// Init http (For PNG stream)
 var http    = require('http');
 
+/****************************************************** 
 
+Drone Camera Streaming API
+
+*******************************************************/
 console.log('Connecting png stream ...');
 
-// var pngStream = arDrone.createClient().getPngStream();
 var pngStream = client.getPngStream();
 
 var lastPng;
@@ -24,53 +29,87 @@ var server = http.createServer(function(req, res) {
     res.end('Did not receive any png data yet.');
     return;
   }
-
+  // Send PNG steam to server at port 8080
   res.writeHead(200, {'Content-Type': 'image/png'});
   res.end(lastPng);
 });
 
+// Listen for PNG image steam
 server.listen(8080, function() {
   console.log('Serving latest png on port 8080 ...');
 });
 
-var callback = function(err) { if (err) console.log(err+'hallo'); else console.log("no problemo");};
-//var callback =  function() {console.log('----!!!!!--!!!!!callback');}
-//client.config({ key: 'control:altitude_max', value: 600, timeout: 10 }, callback);
+// var callback = function(err) { if (err) console.log(err+'hallo'); else console.log("no problemo");};
+// client.config({ key: 'control:altitude_max', value: 600, timeout: 10 }, callback);
 
 // Maximum Altitude 1m
 // Minimum Altitude 20cm
 // Max vertical speed: 200mm/s
 // client.config({key: 'control:altitude_max', value: 600}, callback);
-// client.config('control:altitude_min', 200);
-// //console.log(client.config('control:altitude_max'));
-//client.config('control:altitude_max', '1');
-//client.config('control:control_vz_max', 100);
-//console.log('VZ_max:' + client.config({key:'CONTROL:control_vz_max'}) );
-//client.config('general:navdata_demo', 'TRUE');
-//client.on('navdata', console.log);
 
-// // Minimum Altitude 20cm
-// client.config('control:control_yaw', 1.0);
-// client.config('control:outdoor', false);
-// client.config('control:euler_angle_max', 0.25);
+/****************************************************** 
 
-//client.config({key: 'CONTROL:altitude_max', value: 500, timeout: 500}, callback);
-// client.takeoff();
+Drone Flight API
 
-// client
-// .after(10,function() {
-// 	this.stop();
-// 	this.up(500);
-// })
-// .after(10000, function() {
-//     this.stop();
-//     this.land();
-//   });
+*******************************************************/
 
+// Minimum drone altitude in millimeters.
+// Should be left to default value (50), for control stabilities issues
+client.config('control:altitude_min', 50);
 
-// New
+// Maximum drone altitude in millimeters.
+// On AR.Drone 1.0 : Give an integer value between 500 and 5000 to prevent the drone from ﬂying above this limit,
+// or set it to 10000 to let the drone ﬂy as high as desired. On AR.Drone 2.0 : Any value will be set as a maximum
+// altitude, as the pressure sensor allow altitude measurement at any height. Typical value for "unlimited" altitude
+// will be 100000 (100 meters from the ground)
+client.config('control:altitude_max', 500);
+// console.log(client.config('control:altitude_max'));
 
+// Maximum vertical speed of the AR.Drone, in milimeters per second.
+// Recommanded values goes from 200 to 2000. Others values may cause instability.
+// This value will be saved to indoor/outdoor_control_vz_max, according to the CONFIG:outdoor setting.
+client.config('control:control_vz_max', 100);
+// console.log('VZ_max:' + client.config({key:'CONTROL:control_vz_max'}) );
 
+// The drone can either send a reduced set of navigation data to its clients 
+client.config('general:navdata_demo', 'TRUE');
+client.on('navdata', console.log);
+
+// Maximum yaw speed of the AR.Drone, in radians per second.
+// Recommanded values goes from 40/s to 350/s (approx 0.7rad/s to 6.11rad/s). Others values may cause instability.
+// This value will be saved to indoor/outdoor_control_yaw, according to the CONFIG:outdoor setting 
+client.config('control:control_yaw', 1.0);
+
+// This settings tells the control loop that the AR.Drone is flying outside. Setting the indoor/outdoor flight will load the corresponding indoor/outdoor_control_yaw, indoor/outdoor_eu- ler_angle_max and indoor/outdoor_control_vz_max.
+// Note : This settings enables the wind estimator of the AR.Drone 2.0 , and thus should always be enabled when flying outside. Note : This settings corresponds to the Outdoor flight setting of AR.FreeFlight
+client.config('control:outdoor', false);
+
+// Maximum bending angle for the drone in radians, for both pitch and roll angles.
+// The progressive command function and its associated AT command refer to a percentage of this value. Note : For
+// AR.Drone 2.0 , the new progressive command function is preferred (with the corresponding AT command).
+// This parameter is a positive floating-point value between 0 and 0.52 (ie. 30 deg). Higher values might be available
+// on a specific drone but are not reliable and might not allow the drone to stay at the same altitude.
+// This value will be saved to indoor/outdoor_euler_angle_max, according to the CONFIG:outdoor setting.
+client.config('control:euler_angle_max', 0.25);
+
+client.takeoff();
+
+client
+.after(10,function() {
+	this.stop();
+	this.up(500);
+})
+.after(10000, function() {
+  this.stop();
+  this.land();
+});
+
+/****************************************************** 
+
+Drone Flight API Low Level Control (Not using)
+
+*******************************************************/
+// New (Not using)
 //   var arDrone = require('ar-drone');
 // var control = arDrone.createUdpControl();
 // var start   = Date.now();
